8567455dd116a01082450536315a4a0d9a6a1b7f
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2023-06-20 21:37:57 +0200

    aggiunto delete

--- app/Http/Livewire/TasksTable.php
+++ app/Http/Livewire/TasksTable.php
@@ -7,8 +7,17 @@ use Livewire\Component;
 
 class TasksTable extends Component
 {
+
+    public function destroy(Task $task)
+    {
+        $task->delete(); 
+        session()->flash('tasks', 'Task successfully updated.');
+    }
+
+    
     public function render()
     {
+        
         $tasks = Task::all();
         return view('livewire.tasks-table', compact('tasks'));
     }
